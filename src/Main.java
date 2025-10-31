34eba1f6ee754a1f4d356e7b876f936e6793d0d8
Abdulrahman Rafat Ahmed <140813224+Abdul-Rahman-Rafat@users.noreply.github.com>
2025-10-31 23:20:37 +0200

    4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Abdo Rafat");
+        System.out.println("Hello from Jenkins Java Pipeline!");
     }
 }
